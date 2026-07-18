547f1f726a8e86d6f470c352cf0456cc4e0d822c
sourabhabs <sourabh.bhatt@abs.edu.in>
2026-07-18 12:21:32 +0530

    style: change bottom from 40px to -120px to move timer down to jacket/badge level on desktop

--- app/components/Bba2PreviewShell.tsx
+++ app/components/Bba2PreviewShell.tsx
@@ -858,7 +858,7 @@ export default function Bba2PreviewShell({
           }
           .admissions-timer-desktop-rhs {
             position: absolute;
-            bottom: 40px;
+            bottom: -120px;
             right: 180px !important;
             left: auto !important;
             z-index: 5;
